064a5c1e578679f89077284d59005ba1c3c59095
RobotEby <ubuntu.onion@hotmail.com>
2025-10-22 11:03:09 -0300

    fix: Updated Vite configuration with proper support for SPA routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/tests/setup.ts",
-    css: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: '::',
+    port: 8080,
+    strictPort: true,
+    hmr: {
+      clientPort: 8080,
     },
   },
-});
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+}));
